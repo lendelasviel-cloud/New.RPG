fadf83cf5af3682b31544a66b31856eb5b012dc9
lendelasviel-cloud <lendelasviel@gmail.com>
2026-02-08 08:49:59 +0100

    Update encounters.py

--- Gra/encounters.py
+++ Gra/encounters.py
@@ -1,17 +1,18 @@
 # encounters.py
 import random
 
-def losuj_potwora(wrogowie_lokacji):
-    if not wrogowie_lokacji:
-        return None
-
 def losowe_wydarzenie():
     return random.choice([
         "walka",
         "nic",
-        "skrzynia",
-        "zasadzka"
+        "nic",
+        "nic",
+        "skrzynia"
     ])
+
+def losuj_potwora(wrogowie_lokacji):
+    if not wrogowie_lokacji:
+        return None
 
     return random.choices(
         list(wrogowie_lokacji.keys()),
